674a63b148fe2b6583530c2eb80c8684ed229a20
Eli Baum <ebaum@mitre.org>
2021-11-02 18:59:12 -0400

    full encode/decode working with commas

--- processing/8b10b/8b10b.py
+++ processing/8b10b/8b10b.py
@@ -5,10 +5,12 @@
 
 from enum import Enum, auto
 from collections import defaultdict, namedtuple
+from os import sync
 from typing import NamedTuple, Tuple
 import random
 import sys
 from pprint import pprint
+import itertools, functools
 
 CODEWORD_FILE = '8b10b.tsv'
 
@@ -41,10 +43,15 @@ decodewords = {
 CONTROL_CODES = (0x1C, 0x3C, 0x5C, 0x7C, 0x9C, 0xBC,
                  0xDC, 0xFC, 0xF7, 0xFB, 0xFD, 0xFE)
 
-COMMAS = (0x3C, 0xBC, 0xFC)
+COMMAS = (0x3C, 0xBC) # 0xFC - not using K.28.7 
+
+# these are teh two bit sequences we look for to sync
+COMMA_BIT_SEQUENCES = [
+    [0, 0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 0, 0, 0, 0, 0, 1]
+]
 
 ENC_RUNNING_DISPARITY = -1
-DEC_RUNNING_DISPARITY = -1
 
 def _8b10b_HGF(v: int) -> int:
     return (v >> 5) & 0x7
@@ -192,23 +199,32 @@ def random_test():
 
 SEED = 13
 
+def bits(b, bitlen=CODEWORD_SIZE):
+    # MSB first
+    for i in reversed(range(bitlen)):
+        yield 1 if (b & (1 << i)) else 0
+
 def random_encoder():
     COMMA_EVERY = 32
     c = 0
     enc_rng = random.Random(SEED)
     while True:
         v = enc_rng.randint(0, 0xFF)
-        yield encode_8b10b(CodewordType.DATA, v)
+        enc_rng.seed(v * v)
+
+        yield from bits(encode_8b10b(CodewordType.DATA, v))
         c += 1
 
-        # if c >= COMMA_EVERY:
-        #     yield encode_8b10b(CodewordType.CONTROL, COMMAS[0])
-        #     c = 0
+        if c >= COMMA_EVERY:
+            yield from bits(encode_8b10b(CodewordType.CONTROL, COMMAS[0]))
+            c = 0
 
-def decode_8b10b_symbol(val: int, rd: int = None) -> Tuple[CodewordType, int]:
+DEC_RUNNING_DISPARITY = -1
+
+def decode_8b10b_symbol(val: int) -> Tuple[CodewordType, int]:
     global DEC_RUNNING_DISPARITY
 
-    hi = val >> 4  # 6 bits abcdei
+    hi = val >> 4 # 6 bits abcdei
     dx_opt = decodewords['x'][rd_to_index(DEC_RUNNING_DISPARITY)][hi]
     pc = popcount(hi)
     DEC_RUNNING_DISPARITY += pc - (HI_SIZE - pc)
@@ -248,24 +264,76 @@ def decode_8b10b_symbol(val: int, rd: int = None) -> Tuple[CodewordType, int]:
 
     return (dy[0] << 5) | dx[0], t
 
-def decode_8b10b_stream(stream):
+def decode_8b10b_stream(stream_gen):
+    global DEC_RUNNING_DISPARITY
     dec_rng = random.Random(SEED)
-    n = 0
-    try:
-        for b in stream():
-            v, t = decode_8b10b_symbol(b)
-            n += 1
 
+    stream = stream_gen()
+
+    # eat some random bits
+    for _ in range(random.randint(100, 1000)):
+        next(stream)
+
+    # first, find comma
+
+    SYNC_LEN = len(COMMA_BIT_SEQUENCES[0])
+    sync_window = [0] * SYNC_LEN
+    n_comma = 0
+
+    for b in stream:
+        n_comma += 1
+        
+        sync_window.pop(0)
+        # append new
+        sync_window.append(b)
+
+        if sync_window in COMMA_BIT_SEQUENCES:
+            print("Comma after", n_comma)
+
+            # adjust RD
+            if sync_window == COMMA_BIT_SEQUENCES[0]:
+                # RD was -1, so now 1
+                DEC_RUNNING_DISPARITY = 1
+            else:
+                # was 1, now -1
+                DEC_RUNNING_DISPARITY = -1
+            
+            break
+    
+    # take two more bits to align with next codeword
+    next(stream)
+    next(stream)
+
+    n_ok = 0
+    n_err = 0
+
+    try:
+        for cw_l in itertools.zip_longest(
+                    *[iter(stream)] * CODEWORD_SIZE,   
+                    fillvalue=0
+                ):
+
+            cw = functools.reduce(lambda x, c: (x << 1) | c, cw_l)
+            
+            v, t = decode_8b10b_symbol(cw)
+
+            if t == CodewordType.CONTROL:
+                continue
+            
             exp = dec_rng.randint(0, 0xFF)
+            dec_rng.seed(v * v)
 
             try:
                 assert v == exp
+                n_ok += 1
             except AssertionError:
-                print(f"{n} correct. got {v}, exp {exp}")
-                break
+                print("#")
+                n_err += 1
+                
 
     except KeyboardInterrupt:
-        print(f"\n== {n} correct")
+        print(f"\n== {n_ok} correct")
+        print(f"== {n_err} wrong")
 
 
 def decode_test():
